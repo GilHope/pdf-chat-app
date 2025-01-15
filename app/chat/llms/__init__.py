79217bfcc9eaf94f045928481905fb1a576171fe
Gil <ghopecs@gmail.com>
2025-01-15 12:32:01 -0500

    update

---
+++ app/chat/llms/__init__.py
@@ -0,0 +1,10 @@
+from functools import partial
+from .chatopenai import build_llm
+
+llm_map = {
+    "gpt-4": partial(build_llm, model_name="gpt-4"),
+    "gpt-3.5-turbo": build_llm
+}
+
+builder = llm_map["gpt-4"]
+builder(chat_args)
