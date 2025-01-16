47e162267c4bde05d39ef027b43eb72ab8d07f74
Gil <ghopecs@gmail.com>
2025-01-16 12:41:29 -0500

    def select_component

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -10,38 +10,29 @@ from app.web.api import (
     get_conversation_components
 )
 
-def build_chat(chat_args: ChatArgs):
-    """
-    :param chat_args: ChatArgs object containing
-        conversation_id, pdf_id, metadata, and streaming flag.
-
-    :return: A chain
-
-    Example Usage:
-
-        chain = build_chat(chat_args)
-    """
+def select_compnent(
+    component_type, component_map, chat_args
+):
     components = get_conversation_components(
         chat_args.conversation_id
     )
-    previous_retriever = components["retriever"]
+    previous_component = components[component_type]
 
-    retriever = None
-
-    if previous_retriever:
-        build_retriever = retriever_map[previous_retriever]
-        retriever = build_retriever(chat_args)
-
+    if previous_component:
+        builder = component_map(previous_component)
+        return previous_component, builder(chat_args)
     else:
-        random_retriever_name = random.choice(list(retriever_map.keys()))
-        build_retriever = retriever_map[random_retriever_name]
-        retriever = build_retriever(chat_args)
-        set_conversation_components(
-            conversation_id=chat_args.conversation_id,
-            llm=""
-            memory=""
-            retriever=random_retriever_name
-        )
+        random_name = random.choice(list(component_map.keys()))
+        builder = component_map[random_name]
+        return random_name, builder(chat_args)
+
+def build_chat(chat_args: ChatArgs):
+    retriever_name, retriever = select_component(
+        "retriever",
+        retriever_map,
+        chat_args
+    )
+    
 
     llm = build_llm(chat_args)
     condense_question_llm = ChatOpenAI(streaming=False)
