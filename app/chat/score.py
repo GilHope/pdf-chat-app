9c1a51f57862002457f32cf8ef81d9923380beca
Gil <ghopecs@gmail.com>
2025-01-21 14:51:53 -0500

    test

--- app/chat/score.py
+++ app/chat/score.py
@@ -6,11 +6,11 @@ def random_component_by_score(component_type, component_map):
         raise ValueError(f"Invalid component type")
 
     # From redis, get the hash containing the sum total scores for the given component_type
-
+    values = client.hgetall(f"{component_type}_score_values")
     # From redis, get the hash containing the number of times each component has been voted on
-
+    counts = client.hgetall(f"{component_type}_score_counts")
     # Get all the valid component names from the component map
-
+    print(values, counts)
     # Loop over those valid names and use them to calculate the average score for each
     # Add average score to a dictionary
 
