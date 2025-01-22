d5d61023c34f28a4a55e23cb7759c1c9b5094ebe
Gil <ghopecs@gmail.com>
2025-01-21 20:18:43 -0500

    avgeraging scores

--- app/chat/score.py
+++ app/chat/score.py
@@ -10,9 +10,18 @@ def random_component_by_score(component_type, component_map):
     # From redis, get the hash containing the number of times each component has been voted on
     counts = client.hgetall(f"{component_type}_score_counts")
     # Get all the valid component names from the component map
-    print(values, counts)
+    names = component_map.keys()
+
     # Loop over those valid names and use them to calculate the average score for each
     # Add average score to a dictionary
+    avg_scores = {}
+    for name in names:
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
+
+    print(avg_scores)
 
     # Do a weighted random selection
 
