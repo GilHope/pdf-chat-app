1b586ced29ab6b0d55b2ec852c0450b6153842b0
Gil <ghopecs@gmail.com>
2025-01-23 13:27:14 -0500

    updat init

--- app/chat/memories/__init__.py
+++ app/chat/memories/__init__.py
@@ -2,5 +2,6 @@ from .sql_memory import build_memory
 
 
 memory_map = {
-    "sql_buffer_memory": build_memory
+    "sql_buffer_memory": build_memory,
+    "sql_window_memory": window_buffer_memory_builder
 }
