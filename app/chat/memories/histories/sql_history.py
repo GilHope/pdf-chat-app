aeb9bf6eac32eb694dce4343aa92492627a234d7
Gil <ghopecs@gmail.com>
2025-01-23 13:20:07 -0500

    move class sqlmessagehistory from sql_memory.py to sql_history.py

---
+++ app/chat/memories/histories/sql_history.py
@@ -0,0 +1,16 @@
+class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
+    conversation_id: str
+
+    @property
+    def messages(self):
+        return get_messages_by_conversation_id(self.conversation_id)
+    
+    def add_message(self, message):
+        return add_message_to_conversation(
+            conversation_id=self.conversation_id, 
+            role=message.type,
+            content=message.content
+        )
+    
+    def clear(self):
+        pass
