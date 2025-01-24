8cb7797595d1067f6456c84b65690b5a32074d5e
Gil <ghopecs@gmail.com>
2025-01-24 12:37:18 -0500

    update

--- app/chat/tracing/langfuse.py
+++ app/chat/tracing/langfuse.py
@@ -3,5 +3,6 @@ from langfuse.client import Langfuse
 
 langfuse = Langfuse(
     os.environ.get('LANGFUSE_PUBLIC_KEY'),
-    os.environ.get('LANGFUSE_SECRET_KEY')
+    os.environ.get('LANGFUSE_SECRET_KEY'),
+    host="https://prod-langfuse.fly.dev"
 )
