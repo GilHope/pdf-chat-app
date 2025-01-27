2e3f26fff095fa041c9e3884aabfe4c6237200fb
Gil <ghopecs@gmail.com>
2025-01-27 09:03:12 -0500

    update

--- app/chat/chains/retrieval.py
+++ app/chat/chains/retrieval.py
@@ -1,5 +1,6 @@
 from langchain.chains import ConversationalRetrievalChain
 from app.chat.chains.streamable import StreamableChain
+from app.chat.chains.traceable import TraceableChains
 
 class StreamingConversationalRetrievalChain(
     StreamableChain,
